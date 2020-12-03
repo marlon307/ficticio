75b7003280596ced5d750eca50c5bce415b025dd
Marlon <marlon-ramosb@hotmail.com>
2020-12-03 08:26:43 -0300

    1.0.0.6

---
+++ script.js
@@ -0,0 +1,19 @@
+//Tab active
+function tabsrc() {
+    const tabh = document.querySelectorAll('.mn-user');
+    tabh[0].classList.add('active');
+    if (tabh.length) {
+        function active(index) {
+            tabh.forEach((tab) => {
+                tab.classList.remove('active');
+            });
+            tabh[index].classList.add('active');
+        }
+        tabh.forEach((item, index) => {
+            item.addEventListener('mouseover', () => {
+                active(index);
+            });
+        });
+    }
+}
+tabsrc();
