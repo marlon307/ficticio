cd6b813ae2b636f7b9c27b7d918391ec4355bf32
marlon307 <marlon-ramosb@hotmail.com>
2021-02-16 10:35:14 -0300

    1.0.0.14 load

--- js/modules/addons.js
+++ js/modules/addons.js
@@ -4,4 +4,10 @@ export default function addons() {
     fm.forEach((e) => {
         e.setAttribute('id', 'fv');
     });
+
+    const l = document.querySelectorAll('a');
+    // fv - form validate
+    l.forEach((e) => {
+        e.setAttribute('data-link', '');
+    });
 }
--- js/modules/load.js
+++ js/modules/load.js
@@ -1,34 +1,43 @@
 export default function load() {
 
-    const url = document.querySelectorAll('a');
+    const links = document.querySelectorAll('a');
 
-    url.forEach(l => {
-        l.addEventListener('click', cl);
+    links.forEach(link => {
+        link.addEventListener('click', linklick);
     });
-
-    function cl(e) {
-        e.preventDefault();
-        pg(e.target.href);
-        window.history.pushState(null, null, e.target.href);
-    }
-
-    async function pg(url) {
-        document.querySelector('.content').innerHTML = 'Carregando';
-        const pr = await fetch(url);
-        const pc = await pr.text();
-        lpc(pc);
-    }
-
-    function lpc(ntxt) {
-        const nhtml = document.createElement('main');
-        nhtml.innerHTML = ntxt;
-        const opg = document.querySelector('.content');
-        const npg = nhtml.querySelector('.content');
-        opg.innerHTML = npg.innerHTML;
-        document.title = nhtml.querySelector('title').innerHTML;
-    }
 
     window.addEventListener('popstate', () => {
-        lpc(window.location.href);
+        fetchPage(window.location.href);
     });
+
+    async function fetchPage(url) {
+        document.querySelector('.content').innerHTML = 'Carregando';
+        const pgResponse = await fetch(url);
+        const pgText = await pgResponse.text();
+        rpContent(pgText);
+    }
+
+    function linklick(e) {
+        e.preventDefault();
+
+        if (e.target.href != undefined) {
+            fetchPage(e.target.href);
+            window.history.pushState(null, null, e.target.href);
+        } else {
+            fetchPage(e.target.closest('[data-link]').href);
+            window.history.pushState(null, null, e.target.closest('[data-link]').href);
+        }
+    }
+
+    //rpContent - replace content - Susbistituir conteudo
+    function rpContent(newText) {
+        const newHtml = document.createElement('main');
+        newHtml.innerHTML = newText;
+
+        const oldPg = document.querySelector('.content');
+        const newPg = newHtml.querySelector('.content');
+
+        oldPg.innerHTML = newPg.innerHTML;
+        document.title = newHtml.querySelector('title').innerText;
+    }
 }
